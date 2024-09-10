25174eb1faead661ba022514c9c04377f5194e4a
Ramon Noleto <devnoleto@gmail.com>
2024-09-10 12:30:13 -0300

    :rocket: router: Criando rota de post goals
    Criação de rota para cadastrar meta

---
+++ server/src/functions/create-goal.ts
@@ -0,0 +1,21 @@
+import { AwsDataApiPreparedQuery } from "drizzle-orm/aws-data-api/pg"
+import { db } from "../db"
+import { goals } from "../db/schema"
+
+interface CreateGoalRequest {
+    title: string
+    desiredWeeklyFrequency: number
+}
+
+export async function createGoal({ title, desiredWeeklyFrequency }: CreateGoalRequest){
+    const result = await db.insert(goals).values({
+        title,
+        desiredWeeklyFrequency,
+    }).returning()
+
+    const goal = result[0]
+
+    return {
+        goal,
+    }
+}
