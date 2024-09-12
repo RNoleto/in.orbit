b0d41080c131d476c955394b78fbab28c9ba6b88
Ramon Noleto <devnoleto@gmail.com>
2024-09-12 14:02:27 -0300

    :icon: backup: Backup para continuar em casa
    Ultimo salvamento 55" de video aula 3

--- web/src/components/pending-goals.tsx
+++ web/src/components/pending-goals.tsx
@@ -26,7 +26,7 @@ export function PendingGoals() {
                     <OutlineButton 
                         key={goal.id} 
                         disabled={goal.completionsCount >= goal.desiredWeeklyFrequency}
-                        onClick={handleCompleteGoal}
+                        onClick={() => handleCompleteGoal(goal.id)}
                     >
                         <Plus className="size-4 text-zinc-600" />
                         {goal.title}
